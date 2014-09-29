f8c7ebcbc01f06efdd7825444c62da876cbb890f
mohdsyahrirmuhammad@gmail.com <mohdsyahrirmuhammad@gmail.com@77a063e8-c552-51cb-ee98-2faa20196357>
2014-09-29 04:57:30 +0000

    first commit

---
+++ trunk/simpleCrudWebApplication/src/main/java/com/syahrir/repository/TestStudentRepo.java
@@ -0,0 +1,10 @@
+package com.syahrir.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.syahrir.entity.TestStudent;
+
+public interface TestStudentRepo extends CrudRepository<TestStudent, Integer>{
+
+
+}
